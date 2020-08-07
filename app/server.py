51ce75582f2af1f52b8194b42a41513bf781dc3a
Piyush Agrawal <65438657+Piyush1kgp@users.noreply.github.com>
2020-08-08 01:17:58 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/file/d/1HPQhqysw6Mv-HPsOp2H5OJMQh9psBx32/view?usp=sharing'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Hindus', 'Muslims', 'Sikhs', 'buddhists']
 path = Path(__file__).parent
 
 app = Starlette()
